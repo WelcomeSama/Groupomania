a972ca706f789e53bdb8aa07e7e07f91c40ef9a9
Brandon Vital <bran.vital@gmail.com>
2022-06-02 17:52:17 +0200

    routes user

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const { Sequelize } = require('sequelize');
+
+const userRoutes = require('./routes/user');
+//const postRoutes = require('./routes/post');
+const path = require("path");
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 const mysql = require("mysql");
 
@@ -16,5 +25,25 @@ db.connect(function (err) {
   if (err) throw err;
   console.log("Connecté à la base de données MySQL!");
 });
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use("/api/user", userRoutes);
+//app.use("/api/post", postRoutes);
 
 module.exports = app;
---
+++ backend/migrations/20220528140016-create-like.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Likes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.INTEGER
+      },
+      idPost: {
+        type: Sequelize.INTEGER
+      },
+      idUSERS: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Likes');
+  }
+};
