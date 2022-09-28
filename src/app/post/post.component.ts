c3f7e7b690cfa26faf5f97174763123efe41ed4f
Brandon Vital <brandon.vital@hotmail.fr>
2022-09-28 14:58:47 +0200

    wip

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,34 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { PostsService } from '../services/post.service';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Post } from '../models/post';
-import { Router } from '@angular/router';
-import { catchError, EMPTY, map, Observable, of, switchMap, take, tap } from 'rxjs';
 
 @Component({
-  selector: 'app-post',
+  selector: 'post-brendiche',
   templateUrl: 'post.component.html',
-  styleUrls: ['post.component.css']
+  styleUrls: ['post.component.css'],
 })
-export class PostComponent implements OnInit {
-  posts: Post[] = [];
-  errorMsg!: string;
-  userId!: string;
-  likePending!: boolean;
-  liked!: boolean;
+export class PostBrendicheWay {
+  @Input() post!: Post;
+  @Output() postEvent = new EventEmitter<Post>();
+  @Output() deletPostEvent = new EventEmitter<Post>();
 
-  constructor(
-    private postService: PostsService,
-    private router: Router) { }
-
-  ngOnInit() {
-    this.postService.post$.subscribe((posts) => {
-      this.posts = posts;
-    });
-    this.postService.getPosts();
+  onClickPost() {
+    this.postEvent.emit(this.post);
   }
 
-  onClickPost(id: string) {
-    this.router.navigate(['post', id]);
+  deletePost() {
+    this.deletPostEvent.emit(this.post);
   }
+
 
 }
