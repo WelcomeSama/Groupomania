abf8363e91220606fb0f5c2ecaff7ec38d7c0a4d
Brandon Vital <brandon.vital@hotmail.fr>
2022-09-28 15:53:12 +0200

    details post component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,8 +14,8 @@ import { NavComponent } from './nav/nav.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-//import { PostComponent } from './post-brendiche-way/post-brendiche-way.component';
-import { PostBrendicheWay } from './post/post.component';
+import { PostComponent } from './post/post.component';
+import { EditPostComponent } from './edit-post/edit-post.component';
 
 @NgModule({
   declarations: [
@@ -28,8 +28,8 @@ import { PostBrendicheWay } from './post/post.component';
     NewPostComponent,
     PostListComponent,
     NavComponent,
-    //PostComponent,
-    PostBrendicheWay,
+    PostComponent,
+    EditPostComponent,
   ],
   imports: [
     BrowserModule,
